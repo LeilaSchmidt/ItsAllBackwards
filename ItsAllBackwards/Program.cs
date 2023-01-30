d9e584ad25bafc7dd4ef52624050a54094e63bc1
Leila Schmidt <lnsnani80@gmail.com>
2023-01-30 23:02:44 +0100

    updating the progress
    continuing tomorrow :)

--- ItsAllBackwards/Program.cs
+++ ItsAllBackwards/Program.cs
@@ -2,9 +2,32 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, please enter a word");
+            string userWord = Console.ReadLine();
+            int length = userWord.Length - 1;
+            char[] arr = new char[length];
+            
+            FibonaciCalculations(arr, 0, length-1);
+
+            Console.WriteLine(string.Join(',', arr));
+        }
+
+       
+        static char[] FibonaciCalculations(char[] arr, int start, int end)
+        {
+            while (start != end)
+            {
+                char temp = arr[end];
+                arr[end] = arr[start];
+                arr[start] = temp;
+
+                FibonaciCalculations(arr, start + 1, end - 1);
+
+
+            }
+            return arr;
         }
     }
 }
